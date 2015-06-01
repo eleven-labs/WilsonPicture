bf3ec610a0cf13d955e22d832bbb119d6eb5b465
noel <kania.noel@gmail.com>
2015-06-01 14:35:34 +0200

    Added test for showing the Delete action

--- packages/custom/wilsonpicture/public/controllers/eventController.js
+++ packages/custom/wilsonpicture/public/controllers/eventController.js
@@ -4,12 +4,13 @@
  * Event show controller
  * @template : view.html
  */
-angular.module('mean.wilsonpicture').controller('EventsViewController', ['$scope', '$stateParams', 'Global', 'Events', 'Pictures',
-    function ($scope, $stateParams, Global, Events, Pictures) {
+angular.module('mean.wilsonpicture').controller('EventsViewController', ['$scope', '$stateParams', 'Global', 'Events', 'Pictures', 'MeanUser',
+    function ($scope, $stateParams, Global, Events, Pictures, MeanUser) {
         $scope.global = Global;
         $scope.package = {
             name: 'wilsonpicture'
         };
+        $scope.user = MeanUser.user;
 
         Events.get({
             eventId: $stateParams.eventId
@@ -20,6 +21,7 @@ angular.module('mean.wilsonpicture').controller('EventsViewController', ['$scope
         Pictures.query({eventId: $stateParams.eventId}, function (pictures) {
             $scope.pictures = pictures;
         });
+
 
         $scope.deletePicture = function (picture_id, index) {
             var picture = new Pictures({_id: picture_id});
